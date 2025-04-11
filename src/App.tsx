3f815aba4e8afd7f568b66f338d38ec0ff4f1930
VBPrasad <81baji@gmail.com>
2025-04-11 10:39:57 +0530

    completed delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,29 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/dashboard/Dashboard'
-import NewExpense from './pages/expense/NewExpenses'
-
-import ExpenseReports from './pages/expense/ExpenseReports'
-import ExpenseDetails from './pages/expense/ExpenseDetails'
-import Login from './pages/login/login'
-import Register from './pages/register/Register'
-import Navbar from './components/navbar'
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import NewExpense from "./pages/expense/NewExpenses";
+import ExpenseReports from "./pages/expense/ExpenseReports";
+import ExpenseDetails from "./pages/expense/ExpenseDetails";
+import Register from "./pages/register/Register";
+import Navbar from "./components/Navbar";
+import Login from "./pages/login/Login";
 
 
 const App = () => {
   return (
     <>
-        <Navbar />
-    <Dashboard />
-
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/new" element={<NewExpense />} />
+          <Route path="/reports" element={<ExpenseReports />} /> */}
+          <Route path="/view/:expensesId" element={<ExpenseDetails />} />
+        </Routes>
+      </BrowserRouter>
     </>
-//     <BrowserRouter>
-//     <Routes>
+  );
+};
 
-//     <Route path="/" element={<Dashboard />} />
-
-//     <Route path="/login" element={<Login />} />
-//     <Route path="/register" element={<Register />} />
-
-// <Route path="/new" element={<NewExpense />} />
-// <Route path="/reports" element={<ExpenseReports />} />
-// <Route path="/view/:expensesId" element={<ExpenseDetails />} />
-
-
-
-//     </Routes>
-//     </BrowserRouter>
-  )
-}
-
-export default App
+export default App;
--- src/pages/expense/ExpenseDetails.tsx
+++ src/pages/expense/ExpenseDetails.tsx
@@ -1,53 +1,108 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import CurrencyUtils from "../../utils/CurrencyUtils";
 import DateUtils from "../../utils/DateUtils";
 import useExpenseByExpenseId from "../../hook/useExpenseByExpenseId";
+import ConfirmDialog from "../../components/ConfirmDialog";
+import {  useState } from "react";
+import { deleteExpenseByExpenseId } from "../../services/expense-service";
 
 const ExpenseDetails = () => {
-const { expensesId } = useParams<{ expensesId: string }>();
+  let { expensesId } = useParams<{ expensesId: string }>();
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+  const navigate=useNavigate();
+  
+
 
   if (!expensesId) {
     return <p> Invalide ExpenseId</p>;
   }
 
-  const { expense, errors, isLoading } = useExpenseByExpenseId(expensesId!);
+  const { expense, errors, isLoading, setIsLoading, setErrors } =
+    useExpenseByExpenseId(expensesId!);
+
+  const handleConfirm = () => {
+    setIsLoading(true);
+    deleteExpenseByExpenseId(expensesId)
+      .then((response) => {
+        if( response && response.status){
+        navigate("/")
+      }
+    }  )
+      .catch((error) => setErrors(error.message))
+      .finally(() => {
+        setIsLoading(false);
+        setShowDialog(false);
+      });
+  };
+  const handleCancel = () => {
+    console.log("Cancel is click");
+    setShowDialog(false);
+  };
   return (
     <div>
       {isLoading && <p> Loading...</p>}
       {errors && <p> {errors}</p>}
-      <div>
-        <button> Delete</button>
-        <button> Edit</button>
-        <Link className="btn btn-sm btn-secondary" to="/"> Back </Link >
-
-       
-      </div> 
-      <table>
-        <tr>
-          <td>Name</td>
-          <td>{expense ? expense?.name : "N/A"}</td>
-        </tr>
-        <tr>
-          <td>Amount</td>
-          <td>
+      <div className="container mt-2">
+        <div className="d-flex flex-row-reverse mb-2">
+          <button
+            className="btn btn-sm btn-danger"
+            onClick={() => setShowDialog(true)}
+          >
             {" "}
-            {expense ? CurrencyUtils.formatToINR(expense?.amount) : "N/A"}
-          </td>
-        </tr>
-        <tr>
-          <td>Date</td>
-          <td>{expense ? DateUtils.formatDateString(expense?.date) : "N/A"}</td>
-        </tr>
-        <tr>
-          <td>Category</td>
-          <td>{expense ? expense?.category : "N/A"}</td>
-        </tr>
-        <tr>
-          <td>Note</td>
-          <td>{expense ? expense?.note : "N/A"}</td>
-        </tr>
-      </table>
+            Delete
+          </button>
+          <button className="btn btn-sm btn-warning mx-2"> Edit</button>
+          <Link className="btn btn-sm btn-secondary" to="/">
+            {" "}
+            Back{" "}
+          </Link>
+        </div>
+        <div className="card">
+          <div className="card-body p-3">
+            <table className="table table-bordeless table-responsive">
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <td>{expense?.name}</td>
+                </tr>
+                <tr>
+                  <th>Amount</th>
+                  <td>
+                    {" "}
+                    {expense
+                      ? CurrencyUtils.formatToINR(expense?.amount)
+                      : "N/A"}
+                  </td>
+                </tr>
+                <tr>
+                  <th>Date</th>
+                  <td>
+                    {expense
+                      ? DateUtils.formatDateString(expense?.date)
+                      : "N/A"}
+                  </td>
+                </tr>
+                <tr>
+                  <th>Category</th>
+                  <td>{expense ? expense?.category : "N/A"}</td>
+                </tr>
+                <tr>
+                  <th>Note</th>
+                  <td>{expense ? expense?.note : "N/A"}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <ConfirmDialog
+          title="Confirm Delete"
+          message="Are you sure want to delete this item?"
+          show={showDialog}
+          onCancel={handleCancel}
+          onConfirm={handleConfirm}
+        />
+      </div>
     </div>
   );
 };
